2e80db451d97832f577b62ab62324b2a354660c5
DoctorTakumi <ivan.bonacic12@gmail.com>
2024-10-13 18:40:58 +0200

    Create main.py
    Creating TicTacToe game

---
+++ TicTacToe/main.py
@@ -0,0 +1,110 @@
+class Board:
+    def __init__(self):
+        self.board = [' ', ' ', ' ',
+                      ' ', ' ', ' ',
+                      ' ', ' ', ' ']
+        
+    # method to print the board
+    def print_board(self):
+        print ('\n')
+        print (' ' + self.board[0] + ' | ' + self.board[1] + ' | ' + self.board[2])
+        print ('-----------')
+        print (' ' + self.board[3] + ' | ' + self.board[4] + ' | ' + self.board[5])
+        print ('-----------')
+        print (' ' + self.board[6] + ' | ' + self.board[7] + ' | ' + self.board[8])
+        
+    def update_board(self, position, type):
+        # adding try...except to prevent players select position other than position-1 
+        try:
+            # player select position n, n-1 index is being updated
+            # if position is not filled, update the board
+            if self.board[position-1] == ' ':
+                self.board[position-1] = type
+                return True
+            # if position if already filled, ask user to select another position
+            else:
+                print ("Position already selected. Select another position!")
+                return False
+        except:
+            print ("Invalid position! Select another position!")
+        
+    # adding win conditions
+    # if three same symbols appears in a row - return True
+    def check_winner (self, type):
+        if (self.board[0] == type and self.board[1] == type and self.board[2] == type) or \
+           (self.board[3] == type and self.board[4] == type and self.board[5] == type) or \
+           (self.board[6] == type and self.board[7] == type and self.board[8] == type) or \
+           (self.board[0] == type and self.board[3] == type and self.board[6] == type) or \
+           (self.board[1] == type and self.board[4] == type and self.board[7] == type) or \
+           (self.board[2] == type and self.board[5] == type and self.board[8] == type) or \
+           (self.board[0] == type and self.board[4] == type and self.board[8] == type) or \
+           (self.board[2] == type and self.board[4] == type and self.board[6] == type):
+            return True
+        else:
+            return False
+        
+    # adding draw condition - if all fields are filled and no row is connected
+    # if draw - return True
+    def check_draw(self):
+        if ' ' not in self.board:
+            return True
+        else:
+            return False
+
+class Player:
+    def __init__(self, type):
+        self.type = type  # type refers to "X" or "O"
+        self.name = self.get_name()
+        
+    def get_name(self):
+        if self.type == "X":
+            name = input ("Player selecting X, enter your name: ")
+        else:
+            name = input ("Player selecting O, enter your name: ")
+        return name
+    
+class Game:
+    def __init__(self):
+        # board attribute to store an object of the Board class
+        self.board = Board()
+        
+        # player1 and player2 attributes to store objects for the 1st and 2nd player
+        self.player1 = Player("X")
+        self.player2 = Player("O")
+        
+        # current_player attribute to identify the turn of either player
+        self.current_player = self.player1
+        
+    def play(self):
+        # infinite loop to run the game
+        while True:
+            # adding try...except to remind players that only numbers 1-9 are valid input
+            try:
+                message = f"{self.current_player.name}, enter your position (1-9): "
+                position = int(input(message))
+                
+                # the update_board() method to return True if the user selected position is not filled yet
+                if self.board.update_board(position, self.current_player.type):
+                    self.board.print_board()
+                    
+                    # checking winner each time after the board is updated
+                    if self.board.check_winner(self.current_player.type):
+                        print (self.current_player.name, "wins!")
+                        break
+                    
+                    # checking draw each time after the board is updated
+                    elif self.board.check_draw():
+                        print ("Game is a draw!")
+                        break
+                    
+                    # switching players when board is updated
+                    else:
+                        if self.current_player == self.player1:
+                            self.current_player = self.player2
+                        else:
+                            self.current_player = self.player1
+            except:
+                print ("Invalid input! Enter a number between 1 and 9!")
+                    
+game = Game()
+game.play()
